144b1a7b4004e17f3779970886c83182fba316fe
npulsipher4 <npulsipher4@gmail.com>
2025-05-26 21:44:14 -0700

    wordhunt: add keyboard mode chooser

--- app/src/main/java/com/example/openbubblesextension/wordhunt/WordHuntGame.kt
+++ app/src/main/java/com/example/openbubblesextension/wordhunt/WordHuntGame.kt
@@ -1,13 +1,49 @@
 package com.example.openbubblesextension.wordhunt
 
 import android.content.Context
+import androidx.glance.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
+import androidx.glance.ImageProvider
+import androidx.glance.LocalContext
+import androidx.glance.layout.Box
+import androidx.glance.layout.Row
+import androidx.glance.layout.padding
 import com.example.openbubblesextension.Game
 import com.example.openbubblesextension.R
+import com.example.openbubblesextension.RenderConfigOption
 
 class WordHuntGame : Game {
-    val mode = 1 // Mode 1: 4x4, Mode 2: 5x5 circle shape, Mode 3: 5x5 X shape, Mode 4: 5x5
+    var mode = 1 // Mode 1: 4x4, Mode 2: 5x5 circle shape, Mode 3: 5x5 X shape, Mode 4: 5x5
     override fun getName(): String {
         return "hunt"
+    }
+
+    override fun isConfigurable(): Boolean {
+        return true
+    }
+
+    @Composable
+    override fun Configuration(
+        context: Context?,
+    ) {
+        val actualContext = context?: LocalContext.current
+        val maps = listOf("Map 1", "Map 2", "Map 3", "Map 4")
+        val selectedMode = maps[mode - 1]
+        val keyboardModeImages = arrayOf(R.drawable.wordhunt_kb_mode1, R.drawable.wordhunt_kb_mode2, R.drawable.wordhunt_kb_mode3, R.drawable.wordhunt_kb_mode4)
+        Box(modifier = GlanceModifier.padding(16.dp)) {
+            Row(modifier = GlanceModifier.padding(horizontal = 8.dp)) {
+                for (image in keyboardModeImages) {
+                    Image(ImageProvider(image), "Mode", modifier = GlanceModifier.defaultWeight().padding(horizontal = 8.dp))
+                }
+            }
+            RenderConfigOption(this, "Map", maps, selectedMode)
+        }
+    }
+
+    override fun setConfigOption(name: String, value: String) {
+        mode = value.takeLast(1).toInt()
     }
 
     override fun gameClass(): Class<*> {
