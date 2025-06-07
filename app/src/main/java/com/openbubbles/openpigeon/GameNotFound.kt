7ce3e6371975820521a9feddf27ab690f5265834
Tae Hagen <tae.hagen@gmail.com>
2025-06-06 19:09:59 -0700

    Fix supported dialog

--- app/src/main/java/com/openbubbles/openpigeon/GameNotFound.kt
+++ app/src/main/java/com/openbubbles/openpigeon/GameNotFound.kt
@@ -14,11 +14,13 @@ class GameNotFound : Activity() {
         var name = intent.getStringExtra("DISPLAY_GAME")
         val isGameSupported = intent.getStringExtra("GAME")
         if (isGameSupported != null) {
-            name = "this game mode"
+            name = "this $name mode"
         }
+
+        val warn = if (isGameSupported != null) "This is a missing feature in OpenPigeon. GamePigeon is an unaffiliated app, and is not responsible for providing this paid game mode. Do not ask for a refund.\n\n" else ""
         MaterialAlertDialogBuilder(this, com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog)
             .setTitle("Sorry, we don't support $name!")
-            .setMessage("But we could! OpenPigeon is fully open-source, and we're looking for game developers to contribute their favorite games. If you're interested, find out more on GitHub.")
+            .setMessage("${warn}But we could! OpenPigeon is fully open-source, and we're looking for game developers to contribute their favorite games. If you're interested, find out more on GitHub.")
             .setPositiveButton("Done") { dialog, which ->
                 finishAndRemoveTask()
             }
