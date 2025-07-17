b5025bffcde9dff215f584b8ac8bc6bd8adc2315
ty8447 <32338679+ty8447@users.noreply.github.com>
2025-07-16 21:29:31 -0400

    Import reversi/ from reversi+keyboardpreviewpagination into new‑games

---
+++ app/src/main/java/com/openbubbles/openpigeon/reversi/ReversiGame.kt
@@ -0,0 +1,38 @@
+package com.openbubbles.openpigeon.reversi
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
+import androidx.glance.layout.Box
+import androidx.glance.layout.padding
+import com.openbubbles.openpigeon.Game
+import com.openbubbles.openpigeon.R
+import com.openbubbles.openpigeon.RenderConfigOption
+import com.openbubbles.openpigeon.godot.GodotGameActivity
+
+class ReversiGame : Game {
+    override fun getVersion(): String {
+        return "5"
+    }
+
+    override fun getName(): String {
+        return "reversi"
+    }
+
+    override fun displayName(): String {
+        return "Reversi"
+    }
+
+    override fun gameClass(): Class<*> {
+        return GodotGameActivity::class.java
+    }
+
+    override fun gamePoster(config: Map<String, String>?): Int {
+        return R.drawable.reversi
+    }
+
+    override fun getDefaultReplay(): String {
+        return "board:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0|move:3,1,2|board:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+    }
+}
