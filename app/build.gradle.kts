44caf506c08b643cd385e7e4efa3bf0786a5c1eb
Tae Hagen <tae.hagen@gmail.com>
2026-01-29 18:19:42 -0800

    Fix versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.compose.compiler)
 }
 
 fun getGodotExecutable(project: Project): String {
