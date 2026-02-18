5c708ff84886943b6868896331704a593b0e2a5a
ty8447 <32338679+ty8447@users.noreply.github.com>
2026-02-18 06:49:14 -0500

    updated paintball to fix small debug test resulting in the incorrect target being selected.

--- app/src/main/assets/paintball/paintball_game.gd
+++ app/src/main/assets/paintball/paintball_game.gd
@@ -295,8 +295,9 @@ func _connect_app_plugin_or_dev() -> void:
 
 	# Editor dev payload
 	print("[DEV] Editor hint active, loading sample game data")
-	var DEV_SCENARIO: int = 3
-	var dev_data_1 := '{"isYourTurn": true,"player":"2","myPlayerId":"","player1":"","player2":"","avatar1":"","avatar2":"","game":"paint","tver":"5","ios":"26.2.1","id":"DEV1","replay":"hp1:3,hp2:3,pos1:2,pos2:-1,target1:2,target2:-1"}'
+	var DEV_SCENARIO: int = 1
+	var dev_data_1 := '{"isYourTurn": true,"player":"2","myPlayerId":"","player1":"","player2":"","avatar1":"","avatar2":"","game":"paint","tver":"5","ios":"26.2.1","id":"DEV1"}'
+	var dev_data_2 := '{"isYourTurn": true,"player":"2","myPlayerId":"","player1":"","player2":"","avatar1":"","avatar2":"","game":"paint","tver":"5","ios":"26.2.1","id":"DEV1","replay":"hp1:3,hp2:3,pos1:2,pos2:-1,target1:2,target2:-1"}'
 	var dev_data_3 := '{"isYourTurn": true,"player":"2","myPlayerId":"","player1":"","player2":"","avatar1":"","avatar2":"","game":"paint","tver":"5","ios":"26.2.1","id":"DEV3","replay":"hp1:3,hp2:3,pos1:2,pos2:2,target1:0,target2:0|hp1:2,hp2:3,pos1:2,pos2:1,target1:-1,target2:1"}'
 	var dev_data := dev_data_1
 	if DEV_SCENARIO == 3:
